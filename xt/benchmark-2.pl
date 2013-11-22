271b4dc7201c29648277f06d558cb25a94505308
Oleg Alistratov <zero@cpan.org>
2013-11-22 14:23:36 +0200

    Benchmark results update.

--- xt/benchmark-2.pl
+++ xt/benchmark-2.pl
@@ -56,3 +56,11 @@ sub rand_nd_gen(;@)
 # ------------------------------------------------------------------------------
 1;
 __END__
+
+
+Benchmark: timing 10000000 iterations of adv, adv_p, base, g, g_p...
+       adv: 10 wallclock secs (10.33 usr +  0.00 sys = 10.33 CPU) @ 968054.21/s (n=10000000)
+     adv_p: 11 wallclock secs ( 9.82 usr +  0.00 sys =  9.82 CPU) @ 1018329.94/s (n=10000000)
+      base:  6 wallclock secs ( 5.36 usr +  0.00 sys =  5.36 CPU) @ 1865671.64/s (n=10000000)
+         g:  5 wallclock secs ( 5.83 usr +  0.00 sys =  5.83 CPU) @ 1715265.87/s (n=10000000)
+       g_p:  5 wallclock secs ( 5.84 usr +  0.00 sys =  5.84 CPU) @ 1712328.77/s (n=10000000)
